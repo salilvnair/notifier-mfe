6c9fdf9295e3b2758aed2b9fb89980a4405f9668
Salil V Nair <salilvnair@icloud.com>
2019-12-06 00:23:37 +0530

    import cleanup

--- projects/notifier/src/lib/notifier.module.ts
+++ projects/notifier/src/lib/notifier.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { UpdateNotifierModule } from './core/module/update-notifier.module';
-import { DownloadNotifierModule } from './core/module/download-notifier.module';
 import { InfoNotifierModule } from './core/module/info-notifier.module';
 import { InfoNotifierComponent } from './components/info-notifier/info-notifier.component';
 
 const IMPORT_EXPORT_MODULE_ARRAY = [
-  UpdateNotifierModule,
-  DownloadNotifierModule,
   InfoNotifierModule
 ];
 
